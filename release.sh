3e4168da3976adc84dc45bd94e3a190d1e76284c
Raisa Barreto <rbarreto@stone.com.br>
2018-03-26 12:14:31 -0300

    fourth release

---
+++ release.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+if ! [[ $2 =~ ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$ ]] 
+then
+    echo "Version number must be in x.y.z format" >&2
+    exit 1
+fi
+
+# branch with the code which will be released
+releaseBranch=$1
+
+# v1.0.0, v1.5.2, etc.
+versionLabel=v$2
+
+# establish branch and tag name variables
+masterBranch=master
+
+# checkout to master and gets its last version
+git checkout $masterBranch 
+git pull origin 
+
+echo "\n"
+echo "Got $masterBranch last version."
+echo "\n"
+
+# checkout to release branch, gets its last version and merge master into it to resolve conflicts
+git checkout $releaseBranch
+git pull origin
+git merge $masterBranch
+
+git commit -m "Merge $masterBranch into $releaseBranch"
+
+echo "\n"
+echo "Merged $masterBranch into $releaseBranch"
+echo "\n"
+
+# checkout to master and merge release branch into it
+git checkout $masterBranch
+git merge --no-ff $releaseBranch
+
+git commit -m "Merge $releaseBranch into $masterBranch"
+
+echo "\n"
+echo "Merged release $releaseBranch into $masterBranch"
+echo "\n"
+
+# create tag for new version from -master
+git tag $versionLabel
+git push --tags
+
+echo "\n"
+echo "Created tag $versionLabel into $masterBranch"
+echo "\n"
+
+# remove release branch remotely and locally
+git push -d origin $releaseBranch
+git branch -D $releaseBranch
+
+echo "\n"
+echo "Deleted release $releaseBranch remotely and locally"
+echo "Congrats on your new release!"
+echo "\n"
