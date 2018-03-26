17bd4e294f519bd7584af606e81f8c095ef0231d
Raisa Barreto <rbarreto@stone.com.br>
2018-03-26 13:44:19 -0300

    altera script de release

--- release.sh
+++ release.sh
@@ -8,55 +8,50 @@ then
 fi
 
 # branch with the code which will be released
-releaseBranch=$1
+releaseBranch=$1 
 
 # v1.0.0, v1.5.2, etc.
-versionLabel=v$2
+versionLabel=v$2 
 
 # establish branch and tag name variables
-masterBranch=master
+masterBranch=master 
 
 # checkout to master and gets its last version
-git checkout $masterBranch 
-
-git pull origin 
+git checkout $masterBranch || exit 1
+git pull origin $masterBranch || exit 1
 
 printf "\n"
 echo "Got $masterBranch last version."
 printf "\n"
 
 # checkout to release branch, gets its last version and merge master into it to resolve conflicts
-git checkout $releaseBranch
-
-git pull origin
-
-git merge $masterBranch
+git checkout $releaseBranch || exit 1
+git pull origin $releaseBranch || exit 1
+git merge $masterBranch || exit 1
 
 printf "\n"
 echo "Merged $masterBranch into $releaseBranch"
 printf "\n"
 
 # checkout to master and merge release branch into it
-git checkout $masterBranch
-
-git merge --no-ff $releaseBranch
+git checkout $masterBranch || exit 1
+git merge --no-ff $releaseBranch || exit 1
 
 printf "\n"
 echo "Merged release $releaseBranch into $masterBranch"
 printf "\n"
 
 # create tag for new version from -master
-git tag $versionLabel
-git push --tags
+git tag $versionLabel || exit 1
+git push --tags || exit 1
 
 printf "\n"
 echo "Created tag $versionLabel into $masterBranch"
 printf "\n"
 
 # remove release branch remotely and locally
-git push -d origin $releaseBranch
-
-git branch -D $releaseBranch
+git push -d origin $releaseBranch || exit 1
+git branch -D $releaseBranch || exit 1
 
 printf "\n"
 echo "Deleted release $releaseBranch remotely and locally"
